720b68ab816076eabf33cd3fe34ab1dafd8fef12
FP2003 <filippieleckii@gmail.com>
2025-08-22 01:02:03 +0100

    font added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Play, Fira_Code, Quicksand } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
 
-const inter = Inter({ subsets: ['latin'] })
+const play = Play({ subsets: ['latin'], weight: ['400', '700'] })
+const firaCode = Fira_Code({ subsets: ['latin'], weight: ['300', '400', '500', '600', '700'] })
+const quicksand = Quicksand({ subsets: ['latin'], weight: ['300', '400', '500', '600', '700'] })
 
 export const metadata: Metadata = {
   title: 'Filip Pielecki',
@@ -17,7 +19,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&family=Play:wght@400;700&family=Quicksand:wght@300..700&display=swap" rel="stylesheet" />
+      </head>
+      <body className={`${play.className} ${firaCode.className} ${quicksand.className}`}> 
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
