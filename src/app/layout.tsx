13035eeaa6e0e92d48c3cbfa6010593bda44516b
FP2003 <filippieleckii@gmail.com>
2025-08-26 00:48:16 +0100

    fixed syntax

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,11 +19,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&family=Play:wght@400;700&family=Quicksand:wght@300..700&display=swap" rel="stylesheet" />
-      </head>
       <body className={`${play.className} ${firaCode.className} ${quicksand.className}`}> 
         <ThemeProvider
           attribute="class"
